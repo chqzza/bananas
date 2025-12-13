cb20a1a6dc3ff28b00114652fb90355ae8224691
Charlie Heaven <charlieheaven2000@gmail.com>
2025-12-13 16:23:08 +0000

    changes

---
+++ assets/maps/Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Tileset" tilewidth="32" tileheight="32" tilecount="5000" columns="8">
+ <image source="../../../TILED MAPS/tilemaps/Heartgold.png" trans="ffffff" width="256" height="20000"/>
+</tileset>
